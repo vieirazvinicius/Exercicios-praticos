93ae859d417cf8264d0a5620530a80ec31bd54c8
VINICIUS AZEDIAS VIEIRA <a96198896@rn.senac.local>
2026-07-14 16:44:37 -0300

    docs: Adicionando comentário da função

--- src/config/database.js
+++ src/config/database.js
@@ -1,3 +1,17 @@
-const veiculos = []
+import pg from 'pg'
+import dotenv from "dotenv"
+dotenv.config()
 
-export default veiculos
+const { Pool } = pg
+
+const conexao = new Pool({
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  database: process.env.PGDATABASE,
+})
+
+console.log(await conexao.query('SELECT NOW()'))
+
+export default conexao;
--- src/modules/veiculo/models/veiculo.model.js
+++ src/modules/veiculo/models/veiculo.model.js
@@ -1,62 +1,75 @@
-import veiculos from "../../../config/database.js"
+import conexao from "../../../config/database.js";
 
+class VeiculoModel {
+  static async cadastrar(
+    placa,
+    modelo,
+    marca,
+    ano,
+    cor,
+    quilometragem,
+    status,
+  ) {
+    const dados = { placa, modelo, marca, ano, cor, quilometragem, status };
+    const query = `insert into veiculo(placa, modelo, marca, ano, cor, quilometragem, status) values($1, $2, $3, $4, $5, $6, $7) RETURNING *`;
+    const resultado = await conexao.query(query, dados);
+    return resultado.rows[0];
+  }
 
-class VeiculoModel{
-    static cadastrar(placa, modelo, marca, ano, cor, quilometragem, status){
-        const veiculo = { placa, modelo, marca, ano, cor, quilometragem, status }
-        veiculos.push(veiculo)
-        return veiculo
+  static async listarTodos() {
+    const query = `select * form veiculo`;
+    const resultado = await conexao.query(query);
+    return resultado;
+  }
+
+  static async listarPorPlaca(placa) {
+    const dados = [placa];
+    const query = `select * from veiculo where placa = $1`;
+    const resultado = await conexao.query(query, dados);
+    return resultado;
+  }
+
+  static async editarTotal(placa, novaQuilometragem, novoStatus) {
+    const veiculo = await VeiculoModel.listarPorPlaca(placa);
+
+    if (veiculo.length === 0) {
+      return null;
+    }
+    const dados = [placa, quilometragem, status];
+    const query = `update veiculo
+        set quilometragem = $2, status = $3
+        where placa = $1 returning *;`;
+    const resultado = await conexao.query(query, dados);
+    return resultado.rows[0];
+  }
+
+  static editarParcial(placa, novaQuilometragem, novoStatus) {
+    const veiculo = VeiculoModel.listarPorPlaca(placa);
+
+    if (!veiculo) {
+      return null;
     }
 
-    static listarTodos(){
-        return veiculos
-        }
+    veiculo.quilometragem = novaQuilometragem;
+    veiculo.status = novoStatus;
 
-    static listarPorPlaca(placa){
-        const veiculo = veiculos.find(veiculo => veiculo.placa === placa)
-        return veiculo
+    return veiculo;
+  }
+
+  static excluirPorPlaca(placa) {
+    const index = veiculos.findIndex((veiculo) => veiculo.placa === placa);
+
+    if (index === -1) {
+      return null;
     }
 
-    static editarTotal(placa, novaQuilometragem, novoStatus){
-       const veiculo = VeiculoModel.listarPorPlaca(placa)
+    const veiculoRemovido = veiculos.splice(index, 1);
+    return veiculoRemovido[0];
+  }
 
-       if(!veiculo){
-            return null
-       }
-
-        veiculo.quilometragem = novaQuilometragem
-        veiculo.status = novoStatus
-
-       return veiculo
-    }
-
-    static editarParcial(placa, novaQuilometragem, novoStatus){
-        const veiculo = VeiculoModel.listarPorPlaca(placa)
-
-        if(!veiculo){
-            return null
-        }
-
-        veiculo.quilometragem = novaQuilometragem
-        veiculo.status = novoStatus
-
-        return veiculo
-    }
-
-    static excluirPorPlaca(placa){
-        const index = veiculos.findIndex(veiculo => veiculo.placa === placa)
-
-        if(index === -1){
-            return null
-        }
-
-        const veiculoRemovido = veiculos.splice(index, 1)
-        return veiculoRemovido[0]
-    }
-
-    static excluirTodos(){
-        veiculos.length = 0
-    }
+  static excluirTodos() {
+    veiculos.length = 0;
+  }
 }
 
-export default VeiculoModel
+export default VeiculoModel;
